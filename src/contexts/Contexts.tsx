02e690f66dbae91b18feab5555452f88e853ad93
Marek Novák <marek@novakmarek.cz>
2025-01-02 19:41:05 +0100

    Implement google api

--- src/contexts/Contexts.tsx
+++ src/contexts/Contexts.tsx
@@ -3,6 +3,8 @@ import React, { FC, ReactNode } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CurrentUserProvider } from '../providers/CurrentUserProvider';
 import { DialogProvider } from '../providers/DialogProvider';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '../config/queryClient/queryClient';
 
 interface ContextsProps {
   children?: ReactNode;
@@ -11,9 +13,11 @@ interface ContextsProps {
 const Contexts: FC<ContextsProps> = ({ children }) => {
   return (
     <Router>
-      <CurrentUserProvider>
-        <DialogProvider>{children}</DialogProvider>
-      </CurrentUserProvider>
+      <QueryClientProvider client={queryClient}>
+        <CurrentUserProvider>
+          <DialogProvider>{children}</DialogProvider>
+        </CurrentUserProvider>
+      </QueryClientProvider>
     </Router>
   );
 };
