2412df794947b29bc8ee25d720a8ebd5c01c6373
Marek Novák <marek@novakmarek.cz>
2025-01-03 13:28:40 +0100

    Change api URL

--- src/security/hooks/queries/useCustomAxios.ts
+++ src/security/hooks/queries/useCustomAxios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 export const useCustomAxios = (): AxiosInstance => {
   const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_GOOGLE_API_URI,
+    baseURL: import.meta.env.VITE_API_URL,
   });
 
   return axiosInstance;
